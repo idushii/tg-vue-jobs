1ce3b8c835a9a655c0cad21260df32c7c642e1c1
our ame <you@example.com>
2019-12-08 21:07:09 +0700

    deploy

---
+++ docs/precache-manifest.64aacac7af1293e8f3224bb37c8b97e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fbac935a7f3eb1c659",
+    "url": "css/app.d0bdbd86.css"
+  },
+  {
+    "revision": "0fca30ab6b7b5c05a173",
+    "url": "css/chunk-vendors.29a4a663.css"
+  },
+  {
+    "revision": "e98aa4752084506f1829c9f94efa4c50",
+    "url": "index.html"
+  },
+  {
+    "revision": "6ab026e21683886009e9d7332dcf6875",
+    "url": "items.json"
+  },
+  {
+    "revision": "23fbac935a7f3eb1c659",
+    "url": "js/app.48cab550.js"
+  },
+  {
+    "revision": "0fca30ab6b7b5c05a173",
+    "url": "js/chunk-vendors.6113dec0.js"
+  },
+  {
+    "revision": "6aaddbdfda2b4464d15de6812ac653f6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
