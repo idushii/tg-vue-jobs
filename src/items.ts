0ee8c05ced7d4c8e69102e55be954eb4bbff445c
our ame <you@example.com>
2019-12-08 14:38:47 +0700

    добавил парсинг данных

--- src/items.ts
+++ src/items.ts
@@ -5,40 +5,118 @@ enum Currency {
     Other
 }
 
-class Rating {
-    minPrice: number
-    maxPrice: number
-    currency: Currency
+enum Employment {
+    Полная,
+    Частичная,
+    Проектная,
+    ПоЧасовая
+}
 
-    constructor(str:String) {
-        this.currency = Currency.Rub
-        this.minPrice = 100
-        this.maxPrice = 100
+class Rating {
+    minPrice: number = 100
+    maxPrice: number = 100
+    currency: Currency = Currency.Rub
+
+    constructor(str?: String) {
+        if (str) {
+            let _rating = this.parse(str)
+            this.currency = _rating.currency
+            this.minPrice = _rating.minPrice
+            this.maxPrice = _rating.maxPrice
+        }
+    }
+
+    parse(str: String) {
+        str = str.toLocaleLowerCase()
+        let minPrice = 0, maxPrice = 0, currency = Currency.Rub
+
+        if (str.indexOf('-')! - -1) {
+            let _rating = str.split('-').map(item => item.trim())
+            minPrice = Number.parseInt(_rating[0])
+            let index = _rating[1].indexOf(' ')
+            maxPrice = Number.parseInt(_rating[1].substr(0, index))
+        } else if (str.indexOf(' до ') != -1) {
+            let _rating = str.split(' до ').map(item => item.trim())
+            minPrice = Number.parseInt(_rating[0])
+            let index = _rating[1].indexOf(' ')
+            maxPrice = Number.parseInt(_rating[1].substr(0, index))
+        }
+
+        if (str.indexOf('rub') != -1) currency = Currency.Rub
+        if (str.indexOf('руб') != -1) currency = Currency.Rub
+
+        if (str.indexOf('$') != -1) currency = Currency.Dollar
+        if (str.indexOf('dol') != -1) currency = Currency.Dollar
+        if (str.indexOf('долл') != -1) currency = Currency.Dollar
+
+        if (str.indexOf('euro') != -1) currency = Currency.Euro
+        if (str.indexOf('евро') != -1) currency = Currency.Euro
+        return {
+            minPrice, maxPrice, currency
+        }
+
     }
 }
 
 class Item {
-    City: String
-    Format: String
-    Employment: String 
-    Rating: Rating
+    city: String
+    format: String
+    employment: Employment
+    rating: Rating
 
-    constructor(str:String) {
-        this.City = ''
-        this.Format = ''
-        this.Employment = ''
-        this.Rating = new Rating(str)
+    constructor(str: String) {
+        let item = this.parse(str)
+        this.city = item.city
+        this.format = item.format
+        this.employment = item.employment
+        this.rating = item.rating
     }
 
     private parse(str: String) {
-        return new Item(str)
+        let lines = str.toLocaleLowerCase().split('<br>')
+        let city = '',
+            format = '',
+            employment = Employment.Полная,
+            rating = new Rating();
+
+        for (let line of lines) {
+            if (line.indexOf('город') != -1) {
+                city = line.split(':')[1]
+            }
+            if (line.indexOf('формат') != -1) {
+                format = line.split(':')[1]
+            }
+            if (line.indexOf('занятость') != -1 && line.indexOf('занятость') < 5) {
+                try {
+                    let _employment = line.split(':')[1].toLocaleLowerCase()
+                    if (_employment == 'полная') employment = Employment.Полная
+                    if (_employment == 'fulltime') employment = Employment.Полная
+                    if (_employment == 'частичная') employment = Employment.Частичная
+                    if (_employment == 'partime') employment = Employment.Частичная
+                    if (_employment.indexOf('проект') != -1) employment = Employment.Проектная
+                    if (_employment.indexOf('час') != -1) employment = Employment.ПоЧасовая
+                } catch (err) {
+                    console.log({err})
+                }
+            }
+            if (line.indexOf('вилка') != -1) {
+                rating = new Rating(line.split(':')[1])
+            }
+        }
+
+        return {
+            city,
+            format,
+            employment,
+            rating,
+        }
     }
 }
 
 class Items {
     _items: Item[] = []
 
-    constructor(items?:Item[]) {
+    constructor(items?: Item[]) {
         if (items)
             this._items = items
     }
@@ -52,10 +130,10 @@ class Items {
     }
 
     parse(items: String[]) {
-        for(let item of items) {
+        for (let item of items) {
             this._items.push(new Item(item));
         }
     }
 }
 
-export {Item, Items, Currency, Rating }
+export { Item, Items, Currency, Rating }
