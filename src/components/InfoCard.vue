bac7bf61c41611c7aa6e7623fa1470401741abae
our ame <you@example.com>
2019-12-08 21:46:18 +0700

    Поправил отображениеи для мобильной версии

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="card">
-      <div class="card-content" @click="select">
+  <div class="card">
+    <div class="card-content" @click="select">
+      <template v-if="!isShowText">
         <p>Город: {{City}}</p>
         <p>Формат работы: {{Format}}</p>
         <p>Занятость: {{Employment}}</p>
         <p>Оплата: {{Rating}}</p>
-      </div>
+      </template>
+      <template v-else>
+        <p v-html="Text"></p>
+      </template>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
-import { Items, Item } from '../items';
+import { Component, Prop, Vue, Emit } from "vue-property-decorator";
+import { Items, Item } from "../items";
 
 @Component
 export default class InfoCard extends Vue {
-
   public isShowText: boolean = false;
   @Prop() private City!: string;
   @Prop() private Format!: string;
@@ -23,6 +27,12 @@ export default class InfoCard extends Vue {
   @Prop() private Rating!: string;
   @Prop() private Text!: string;
 
+  @Emit("select")
+  select() {
+    if (window.innerWidth < 600) {
+      this.isShowText = !this.isShowText;
+    }
+  }
 }
 </script>
 
@@ -30,7 +40,7 @@ export default class InfoCard extends Vue {
 <style scoped lang="scss">
 .card {
   cursor: pointer;
-  transition: all .3s;
+  transition: all 0.3s;
   &:hover {
     background-color: rgb(218, 208, 208);
   }
